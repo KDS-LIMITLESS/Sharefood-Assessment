5ef45063039a950c4333483db0e79b4f55909b77
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-10-28 21:52:30 +0100

    Checking for date incosistencies before running calculate_age function

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,12 +1,16 @@
 import {Request, Response, NextFunction} from 'express'
+import { convertToDate } from '../controllers/dob'
 
 
 export async function checkQueryParam(req:Request, res:Response, next:NextFunction) {
-    // type cast ?dob string to type number
-    const params: number = +req.query.dob!
-    // check if number is a valid integer
-    if (!Number.isInteger(params) ) 
-        return res.status(400).send(` dob is not a valid date combination`)
-
-    next()
+    const params = req.query.dob
+    let x = convertToDate(params)  
+    let date:any =  new Date(+req.query.dob!)  
+   
+    // check if number is a valid date 
+    if ( !isNaN(x.getFullYear()) || !isNaN(date))  
+        next();
+    else {
+        return res.status(400).send(` dob is not a valid date combination`);
+    }
 }
