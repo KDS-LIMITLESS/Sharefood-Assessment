58f143fa457bbb4c1f66c15630c90684cfd20889
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-10-28 10:34:34 +0100

    Added comments to code

--- src/controllers/dob.ts
+++ src/controllers/dob.ts
@@ -1,31 +1,31 @@
 import { Request, Response } from 'express';
 
 
-
 class DOB {
 
     async calculate_age(req:Request, res:Response) {
         let date_today =  new Date;
         let birth_date: string | any, age;
 
+        // check if ?dob integer is a valid date format of yyyymmdd
         if (req.query.dob?.length === 8){
-            
             birth_date = convertToDate(req.query.dob)
+            // subtract date today from given dob
             age = date_today.getFullYear() - birth_date.getFullYear()
-
+            
             return res.status(200).json({current_age: age})
         }
 
+        // type cast query parameter to integer
         birth_date = new Date(+req.query.dob!)
-
+        // subtract date today from given dob
         age = date_today.getFullYear() - birth_date.getFullYear();
 
         return res.status(200).json({current_age: age})
-    }
-    
+    }   
 }
 
-
+// function for converting giving string to a valid date string
 function convertToDate (dateString: string | any): Date {
     let formatedString = dateString.slice(0, 4) + '-' + dateString.slice(4, 6) + '-' + dateString.slice(6, 8);
     const dateObj = new Date(formatedString) 
--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -2,7 +2,9 @@ import {Request, Response, NextFunction} from 'express'
 
 
 export async function checkQueryParam(req:Request, res:Response, next:NextFunction) {
+    // type cast ?dob string to type number
     const params: number = +req.query.dob!
+    // check if number is a valid integer
     if (!Number.isInteger(params) ) 
         return res.status(400).send(` dob is not a valid date combination`)
 
