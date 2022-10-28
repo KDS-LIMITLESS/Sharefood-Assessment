924c9a36ebf278864e739576bf33a61ea76cce41
KDS-LIMITLESS <limitlesskds@gmail.com>
2022-10-28 11:11:06 +0100

    Server entrypoint

--- server.ts
+++ server.ts
@@ -7,11 +7,11 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(limiter)
-app.use('/api', require('./src/routes/dob'))
+app.use('', require('./src/routes/dob'))
 
 
 
-const PORT = 3000 || process.env.PORT 
+const PORT = process.env.PORT || 3301
 
 app.listen(PORT, async() => {
     console.log(`Server listening on port ${PORT}`)
